07f47f362749536ef232a706e90761cd20dcb1b6
carlose109-ops <carloseduardot109@gmail.com>
2026-05-31 22:42:56 -0600

    Add files via upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL; // <--- IMPORTANTE
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        URL::forceScheme('https');
+        // Fuerza HTTPS en toda la aplicación cuando esté en producción
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }

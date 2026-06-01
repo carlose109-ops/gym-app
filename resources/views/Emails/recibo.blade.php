88affe686141ed8a3beed08f8b492802a3fa3cb4
carlose109-ops <carloseduardot109@gmail.com>
2026-06-01 09:37:17 -0600

    Add files via upload

--- resources/views/Emails/recibo.blade.php
+++ resources/views/Emails/recibo.blade.php
@@ -11,7 +11,7 @@
 <body>
     <div class="caja">
         <div class="header">IRON GYM</div>
-        <p>Hola <strong>{{ Auth::user()->name }}</strong>,</p>
+        <p>Hola <strong>{{ $userName }}</strong>,</p> 
         <p>¡Gracias por tu compra! Hemos recibido tu pedido y lo estamos procesando.</p>
         
         <h3>Detalles de tu orden:</h3>
@@ -19,16 +19,16 @@
             @php $total = 0; @endphp
             @foreach($carrito as $item)
                 @php $total += $item['precio'] * $item['cantidad']; @endphp
-                <li>{{ $item['nombre'] }} (x{{ $item['cantidad'] }}) - ${{ number_format($item['precio'] * $item['cantidad'], 2) }}</li>
+                <li>{{ $item['nombre'] }} (x{{ $item['cantidad'] }}) - ${{ number_format($item['precio'] * $item['cantidad'], 2) }} MXN</li>
             @endforeach
         </ul>
         
-        <p class="total">Total pagado: ${{ number_format($total, 2) }} MXN</p>
+        <p><strong>Dirección de envío:</strong> {{ $direccion }}</p>
         
-        <p>Tu pedido será enviado a: <em>{{ $direccion }}</em></p>
+        <div class="total">Total Pagado: ${{ number_format($total, 2) }} MXN</div>
         
-        <p>Si elegiste pago en efectivo, recuerda presentar tu número de orden en tu sucursal más cercana.</p>
-        <p>¡Sigue entrenando sin límites!</p>
+        <hr style="border: 0; border-top: 1px solid #ccc; margin-top: 20px;">
+        <p style="font-size: 12px; color: #777; text-align: center;">Este es un correo automático, por favor no respondas a este mensaje.</p>
     </div>
 </body>
 </html>
